4561100a9d53eed98790aca9d7c3680dcbc8c88b
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-16 01:26:54 +0800

    add some tips in pages_controller, modified the new layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,20 @@
 class PagesController < ApplicationController
-  def home
-  end
+	# to make all views in this controller has the same layout: 
+	# layout 'layout_name'
+	# if we didn't setting which layout will render,
+	# it will redner application.html.erb
 
-  def about
-  	@name = "#{params[:first_name]} #{params[:last_name]}"
-  end
+    def home
+    	# to make one method has layout:
+    	# render layout: 'layout_name'
+    end
 
-  def math
-  	@numbers = (1..46).to_a.sample(6)
-  end
+    def about
+        @name = "#{params[:first_name]} #{params[:last_name]}"
+        render layout: 'admin'
+    end
+
+    def math
+        @numbers = (1..46).to_a.sample(6)
+    end
 end
