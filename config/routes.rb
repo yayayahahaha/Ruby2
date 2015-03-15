9e538438c2e478d2568e1031b20cdf9777cab7f5
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-16 02:42:41 +0800

    main diff: add tag helper in _navbar.html.erb
    change admin.html.erb to make it has link to home page
    change math.html.erb to make it readable
    change routes.rb "about" method path to ":me"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'home', 'math', controller: 'pages'
-  get 'about', controller: 'pages', path: :home
+  get 'about', controller: 'pages', path: :me
 
   # the " path: :method_name " can easily change the link we set in every actions which have the link
 
