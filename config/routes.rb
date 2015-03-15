37444f858a9f494657636f0e06dd5548bd979a5b
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-16 02:31:24 +0800

    add a tips about path_helper in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'home', 'math', controller: 'pages'
   get 'about', controller: 'pages', path: :home
 
+  # the " path: :method_name " can easily change the link we set in every actions which have the link
+
 end
