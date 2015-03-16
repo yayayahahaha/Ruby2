363c008eaed1a7c59c46b32e9f0ee2b7a4c7d7e8
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-16 13:13:48 +0800

    try use form to pass a variable to another pages
    add some stlye in assets/stlyesheets/application.css
    tips the @name comes from
    add a new form
    add a new navbar in _varbar.html.erb
    modify config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  get 'home', 'math', controller: 'pages'
-  get 'about', controller: 'pages', path: :me
+  get :home, :math, :form, :about, controller: 'pages'
+
+  get :about, controller: 'pages', path: :me
 
   # the " path: :method_name " can easily change the link we set in every actions which have the link
 
