2b81d60435305a748a41ba9d4ee3b648b6dd5f91
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-17 12:39:38 +0000

    create new migration file to add a column "about" to "user" model
    the method in migration file is start at "add_column",
    then type command "rake db:migrate" to migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317122901) do
+ActiveRecord::Schema.define(version: 20150317123335) do
 
   create_table "posts", force: :cascade do |t|
     t.text    "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150317122901) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "about"
   end
 
 end
