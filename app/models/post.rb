b16be81acd20ea8e3efc08493e448fc2950a820c
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-17 13:16:08 +0000

    add a callback to set content from title if content is nil

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,20 @@
 class Post < ActiveRecord::Base
   validates :title, :content, presence: true
   validate :title_must_start_with_x
+  before_validation :set_content_from_title
+
+
+  def title_must_start_with_x
+    unless title.to_s.start_with? 'X'
+      errors.add :title, 'must start with "X"'
+    end
+  end
+
+  def set_content_from_title
+    self.content = title if content.blank?
+  end
+
+
 
 =begin
   validate do
@@ -11,10 +25,5 @@ class Post < ActiveRecord::Base
   end
 =end
 
-  def title_must_start_with_x
-    unless title.to_s.start_with? 'X'
-      errors.add :title, 'must start with "X"'
-    end
-  end
   
 end
