d18856dd0c516b81f537f3a26a3de5dbeae26ec9
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-17 13:04:06 +0000

    create a custom validation situation
    two ways: a block or a object

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ActiveRecord::Base
   validates :title, :content, presence: true
+  validate :title_must_start_with_x
+
+=begin
+  validate do
+    unless title.to_s.start_with? 'X'
+      errors.add :title, 'must start with "X"'
+    end
+  end
+=end
+
+  def title_must_start_with_x
+    unless title.to_s.start_with? 'X'
+      errors.add :title, 'must start with "X"'
+    end
+  end
+  
 end
