40d6598ff1cf70e8ff971544b2f7d2252c0d3a2e
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-23 02:18:04 +0800

    add a reference between user and posts
    the user has many posts,
    and a post belongs to a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,16 +2,18 @@
 class Post < ActiveRecord::Base
 	validates :title, :content, presence: true
 	before_validation :set_content_from_title
+	validate :title_must_start_with_T
+	
 	def set_content_from_title
 		self.content = title if content.blank?
 	end
 
-
-	validate :title_must_start_with_T
 
 	def title_must_start_with_T
 		unless title.to_s.start_with? 'T'
 			errors.add(:title, 'must start with \"T\"')			
 		end
 	end
+
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150322174624) do
+ActiveRecord::Schema.define(version: 20150322180923) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
