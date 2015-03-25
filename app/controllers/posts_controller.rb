068e184417f3bc64ddcf8068a98cf96f7c5f14f3
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-03-26 04:09:57 +0800

    add method "create", rather than "new"
    add some code into controller "posts"
    create content of create view
    change layout of index.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,17 @@ class PostsController < ApplicationController
   	@post = Post.find params[:id]
   end
 
-  def new
+  # why we have to change method "new" into "create"? isn't method "new" came more earlier than method "create"?
+  def create
+    #we can get title and content value from view, use these two parameters initialize Post object
+    @post = Post.new title: params[:title], content: params[:content]
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+
   end
 
   def edit
